db0d97efe4d7d016e81eba473038c98ac1b906fd
pedrocutrim <pedrocutrim@sefaz.pi.gov.br>
2025-04-11 13:27:49 -0300

    inicial

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Paulo Neto Construções',
+  description: 'Criado por Pedro Cutrim dos Santos',
   generator: 'v0.dev',
 }
 
